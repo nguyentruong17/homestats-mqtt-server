d9c0fecb64bec1795f18e7809350b81d1cf2c40f
Nguyen Truong <42560729+nguyentruong17@users.noreply.github.com>
2023-01-02 23:55:28 +0000

    fix: added depends_on for database

--- database/app.py
+++ database/app.py
@@ -12,6 +12,7 @@ def on_message(client, userdata, msg):
 
 
 # Initialize the client that should connect to the Mosquitto broker
+print('Before Init MQTT')
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
