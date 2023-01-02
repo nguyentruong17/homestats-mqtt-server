48024bf8f3d867cb5f6d805d6844cafb1699ef03
Nguyen Truong <42560729+nguyentruong17@users.noreply.github.com>
2023-01-02 23:43:57 +0000

    feat: adde python script

---
+++ database/app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import paho.mqtt.client as mqtt
+
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    client.subscribe("bedroom/#")
+
+def on_message(client, userdata, msg):
+    message=msg.payload.decode("utf-8")
+    print(client,userdata, message)
+
+
+# Initialize the client that should connect to the Mosquitto broker
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect("192.168.0.62", 1883, 60)
+
+# Blocking loop to the Mosquitto broker
+client.loop_forever()
