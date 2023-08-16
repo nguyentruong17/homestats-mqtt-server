40cfd3eda60d50c95012ee54e11feb4fb0d08f43
nguyentruong17 <nguyentruong17@augustana.edu>
2023-08-15 21:41:50 -0500

    fix: fixed writerecord to aws for multi-measure

--- database/app.py
+++ database/app.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 import boto3
 from botocore.config import Config
-import configparser
+# import configparser
 from datetime import datetime
 import json
 import os
 import paho.mqtt.client as mqtt
-import re
+# import re
 import sqlite3
 
 # PATH
@@ -56,6 +56,7 @@ SENSOR_NAMES_SET = {
     'temp_hdd__hdd1',
     'temp_hdd__hdd2'
 }
+SORTED_SENSORS_LIST = sorted(list(SENSOR_NAMES_SET))
 
 def print_rejected_records_exceptions(err):
     print("RejectedRecords: ", err)
@@ -68,7 +69,7 @@ def get_aws_write_client():
     profile_name=os.environ['AWS_PROFILE']
     
     session = boto3.Session(profile_name=profile_name)
-        
+
     write_client = session.client(
             'timestream-write',
             region_name=REGION_NAME,
@@ -80,6 +81,26 @@ def get_aws_write_client():
     
     return write_client
 
+def prepare_common_attributes():
+    common_attributes = {
+        'Dimensions': [
+            {'Name': 'hostname', 'Value': 'Torrent7'}
+        ],
+        'MeasureName': 'metric',
+        'MeasureValueType': 'MULTI'
+    }
+    
+    return common_attributes
+
+def prepare_measure(measure_name, measure_value):
+    measure = {
+        'Name': measure_name,
+        'Value': str(measure_value),
+        'Type': 'DOUBLE'
+    }
+    
+    return measure
+
 def write_records(rows):
     write_client = get_aws_write_client()
 
@@ -88,45 +109,45 @@ def write_records(rows):
     records = []
 
     for row in rows:
-        # print(row)
-        sqlite_id, timestamp, rgroup, sensor, measure_name, measure_value = row
+        sqlite_id, timestamp, *rest = row
         
         dt_obj = datetime.strptime(timestamp, '%Y-%m-%d %H:%M:%S.%f')
         time = int(round(dt_obj.timestamp() * 1000))
         
-        dimensions = [
-            {'Name': 'rgroup', 'Value': rgroup},
-            {'Name': 'sensor', 'Value': sensor} 
-        ]
-        
         record = {
-            'Dimensions': dimensions,
-            'MeasureName': measure_name,
-            'MeasureValue': str(measure_value),
-            'MeasureValueType': 'MULTI',
-            'Time': str(time)
+            'Time': str(time),
+            'MeasureValues': []
         }
         
-        print(record)
-        
+        for index in range(len(SORTED_SENSORS_LIST)):
+            metric = SORTED_SENSORS_LIST[index]
+            value = rest[index]
+            measure = prepare_measure(metric, value)
+            
+            record['MeasureValues'].append(measure)
+              
         records.append(record)
-        
+     
+    print(f'numrecords: {len(records)}')
     if (len(records) == 0):
         print('No record to write.')
         return
     
-    print('Writing records')
+    print(f'lastrecod: {records[-1]}')
     
     # https://www.geeksforgeeks.org/break-list-chunks-size-n-python/
     batches = [records[i * MAX_RECORDS_PER_WRITE:(i + 1) * MAX_RECORDS_PER_WRITE] for i in range((len(records) + MAX_RECORDS_PER_WRITE - 1) // MAX_RECORDS_PER_WRITE )]
-    
+    print(f'numbatches: {len(batches)}')
+    common_attributes = prepare_common_attributes()
+
+    print('WriteRecords in progress...')    
     for batch in batches: 
         try:
             result = write_client.write_records(
                 DatabaseName=DATABASE_NAME,
                 TableName=TABLE_NAME,
                 Records=batch,
-                CommonAttributes={}
+                CommonAttributes=common_attributes
             )
             print("WriteRecords Status: [%s]" % result['ResponseMetadata']['HTTPStatusCode'])
         except write_client.exceptions.RejectedRecordsException as err:
@@ -139,30 +160,36 @@ def on_connect(client, userdata, flags, rc):
 
     client.subscribe(TOPIC)
     
-    # db_conn = userdata['db_conn']
-    # sql = "SELECT * FROM sensors_data WHERE timestamp >= datetime('now', '-1 hour')"
-    # cursor = db_conn.cursor()
-    # cursor.execute(sql)
+    db_conn = userdata['db_conn']
+    sql = f"SELECT * FROM {SQLITE_TABLE_NAME} WHERE timestamp >= datetime('now', '-2 hour')"
+    cursor = db_conn.cursor()
+    cursor.execute(sql)
     
-    # rows = cursor.fetchall()
+    rows = cursor.fetchall()
     
-    # write_records(rows)
+    print(f'numrows: {len(rows)}')
+    print('WriteRecords started')
+    write_records(rows)
+    print('WriteRecords completed')
+    # db_conn.commit()
+    # cursor.close()
 
 def on_disconnect(client):
     print('Disconnected')
 
 def on_message(client, userdata, msg):
     message=msg.payload.decode('utf-8')
-    # print(message)
     
     payloadJson = json.loads(message)
     timestamp = payloadJson['sent']
+    delimitter = payloadJson['delimitter']
     sensors = payloadJson['payload']
+    
+    print(f'message received at: {timestamp}')
     
     db_conn = userdata['db_conn']
     
     sensorsDict = dict.fromkeys(SENSOR_NAMES_SET, -1)
-    sensorsDict['timestamp'] = timestamp
     
     for each in sensors:
         key = each['Id']
@@ -171,10 +198,9 @@ def on_message(client, userdata, msg):
         if key in sensorsDict:
             sensorsDict[key] = value
     
-    sensors = list(SENSOR_NAMES_SET)
-    cols = ', '.join(sensors)
+    cols = ', '.join(SORTED_SENSORS_LIST)
     
-    vals = map(lambda col: ':' + col, sensors)
+    vals = map(lambda col: ':' + col, SORTED_SENSORS_LIST)
     vals = ', '.join(vals)
     
     sql = f"""INSERT INTO {SQLITE_TABLE_NAME}
@@ -188,14 +214,14 @@ def on_message(client, userdata, msg):
         )
     """
 
+    sensorsDict['timestamp'] = timestamp
     cursor = db_conn.cursor()
     cursor.execute(sql, sensorsDict)
     db_conn.commit()
     cursor.close()
 
 def main():
-    cols = list(SENSOR_NAMES_SET)
-    cols = map(lambda col: col + ' REAL NOT NULL', cols)
+    cols = map(lambda col: col + ' REAL NOT NULL', SORTED_SENSORS_LIST)
     sql = ', '.join(cols)
     
     # Initialize SQLITE3
@@ -209,7 +235,7 @@ def main():
     """
     
     cursor = db_conn.cursor()
-    cursor.execute(f'DROP TABLE IF EXISTS {SQLITE_TABLE_NAME}')
+    # cursor.execute(f'DROP TABLE IF EXISTS {SQLITE_TABLE_NAME}')
     cursor.execute(sql)
     cursor.close()
 
